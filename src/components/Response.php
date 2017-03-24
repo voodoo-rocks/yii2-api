7fb28fd8adbae32beb483eb93b49122f57f89410
Alexander Kryshtalev <alex@voodoo-mobile.com>
2017-03-24 06:02:46 +0000

    [*] Changed the way of generating documentation. It was moved to the proper place

--- src/components/Response.php
+++ src/components/Response.php
@@ -9,31 +9,21 @@
 namespace vr\api\components;
 
 
-use vr\api\controllers\DocController;
-use vr\api\Module;
-use Yii;
-
+/**
+ * Class Response
+ * @package vr\api\components
+ */
 class Response extends \yii\web\Response
 {
+    /**
+     *
+     */
     public function init()
     {
         parent::init();
 
         $this->on(self::EVENT_BEFORE_SEND, function ($event) {
             $response = $event->sender;
-
-            if ($response->format == Response::FORMAT_HTML) {
-                $route = \Yii::$app->requestedRoute;
-
-                /** @var Harvester $harvester */
-                $module = \Yii::$app->controller->module;
-                $harvester = Yii::$app->controller->module->get('harvester');
-                $action = $harvester->findAction($module, $route);
-
-                if ($action) {
-                    $response->data = $this->renderDocView($route);
-                }
-            }
 
             if ($response->format == Response::FORMAT_JSON) {
                 if (!$response->data) {
@@ -50,22 +40,5 @@ class Response extends \yii\web\Response
                 }
             }
         });
-    }
-
-    private function renderDocView($route)
-    {
-        /** @var Harvester $harvester */
-        $harvester = Yii::$app->controller->module->get('harvester');
-
-        /** @var Module $module */
-        $module = \Yii::$app->controller->module;
-
-        $controllers = $harvester->getControllers($module);
-
-        return (new DocController('doc', $module))->render('@api/views/doc/view', [
-            'controllers' => $controllers,
-            'model' => $harvester->findAction($module, $route),
-            'includeMeta' => Yii::$app->session->get('include-meta', false),
-        ]);
     }
 }
